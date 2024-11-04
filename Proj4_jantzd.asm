6f06ad38d84cabd448dd43ce738e75fbdeb63438
David Jantz <dljantz@gmail.com>
2024-11-04 12:32:33 -0600

    finished easy stuff... intro, instructions, farewell

--- Proj4_jantzd.asm
+++ Proj4_jantzd.asm
@@ -14,6 +14,12 @@ INCLUDE Irvine32.inc
 
 .data
 
+	introduction1		BYTE	"Pascal's Triangle Generator -- created by David Jantz",13,10,0
+	introduction2		BYTE	"This program will generate a Pascal's Triangle at the row depth you specify!",13,10,0
+	instructions		BYTE	"Enter an integer from 1 to 13, inclusive. Then sit back and watch the magic unfold.", 13,10,0
+	goodbyeMessage		BYTE	"Thanks for playing! Have a fantabulous day.",13,10,0
+
+
 .code
 main PROC
 	CALL	introduction
@@ -26,18 +32,37 @@ main PROC
 main ENDP
 
 introduction PROC
+	MOV		EDX, OFFSET introduction1
+	CALL	WriteString
+	MOV		EDX, OFFSET introduction2
+	CALL	WriteString
+	CALL	CrLf
+	RET
 introduction ENDP
 
 displayInstructions PROC
+	MOV		EDX, OFFSET instructions
+	CALL	WriteString
+	RET
 displayInstructions ENDP
 
 getUserInput PROC
+	; call readint
+	; move that value into a variable
+	; data validation -- check if value is in correct range. use sub-procedure???
+	; loop if needed
+	RET
 getUserInput ENDP
 
 printPascalTriangle PROC
+	RET
 printPascalTriangle ENDP
 
 farewell PROC
+	CALL	CrLf
+	MOV		EDX, OFFSET goodbyeMessage
+	CALL	WriteString
+	RET
 farewell ENDP
 
 
