6d7f615ba175bd7c8feaa58e16e05c120f1a8052
David Jantz <dljantz@gmail.com>
2024-11-04 11:47:36 -0600

    initial commit

---
+++ Proj4_jantzd.asm
@@ -0,0 +1,31 @@
+TITLE Pascal Triangle Generator     (Proj4_jantzd.asm)
+
+; Author: David Jantz
+; Last Modified: 11/4/2024
+; OSU email address: jantzd@oregonstate.edu
+; Course number/section:   CS271
+; Project Number: 4                Due Date: 11/17/2024
+; Description: This program generates a Pascal triangle for a desired number of 
+;				rows entered by the user up to 13 rows.
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
